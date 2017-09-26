2419ec1d3fd35c4b6450315f1d689b916e3a1696
Saritha Rajamanickam <saritha.r@KGFSL.COM>
2017-09-26 17:25:32 +0530

    first commit

---
+++ src/test/java/com/example/demo/EmployeeBuilder.java
@@ -0,0 +1,41 @@
+/*package com.example.demo;
+
+public class EmployeeBuilder {
+ private Employee employee = new Employee();
+
+ public EmployeeBuilder id(int id) {
+   employee.setId(id);
+ return this;
+ }
+
+ public EmployeeBuilder empname(String empname) {
+   employee.setEmpname(empname);
+ return this;
+ }
+ public EmployeeBuilder designation(String designation)
+ {
+ employee.setDesignation(designation);
+ return this;
+ }
+
+ public EmployeeBuilder email(String email)
+ {
+ employee.setEmail(email);
+ return this;
+ }
+
+ public EmployeeBuilder phone(String phone)
+ {
+ employee.setPhone(phone);
+ return this;
+ }
+ public EmployeeBuilder address(String address)
+ {
+ employee.setAddress(address);
+ return this;
+ }
+
+ public Employee build() {
+ return employee;
+ }
+}*/
